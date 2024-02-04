d85f6aa93a051fe7e4b1ed5cf7b7c04ed6c0c9cb
gentom231 <gzarari@gmail.com>
2024-02-04 20:13:35 +0100

    Ndryshimet per ToDoList u rregulluan

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 
 
+
 window.addEventListener('load', () => {
     let form = document.querySelector("#new-task-form");
     let input = document.querySelector("#new-task-input");
     let list_el = document.querySelector("#tasks");
+
+
 
     form.addEventListener('submit', (e) => {
         e.preventDefault();
@@ -14,40 +17,28 @@ window.addEventListener('load', () => {
             return;
         }
 
-        let task_el = document.createElement("div");
-        task_el.classList.add("task");
+        let task_el = createTaskEl();
 
-        let task_content_el = document.createElement("div");
-        task_content_el.classList.add("content");
+        let task_content_el = createTaskContentEl();
 
         task_el.appendChild(task_content_el);
 
-        let task_input_el = document.createElement('input');
-        task_input_el.classList.add("text");
-        task_input_el.type = "text";
-        task_input_el.value = task;
-        task_input_el.setAttribute("readonly", "readonly");
+        let task_input_el = createTaskInputElement(task);
 
         task_content_el.appendChild(task_input_el);
 
 
-        let task_actions_el = document.createElement("div");
-        task_actions_el.classList.add("actions");
+        let task_actions_el = createTaskActionEl();
 
 
-        let task_edit_el = document.createElement("button");
-        task_edit_el.classList.add("edit");
-        task_edit_el.innerHTML = "Edit";
+        let task_edit_el = createTaskEditEl();
 
-        let task_delete_el = document.createElement("button");
-        task_delete_el.classList.add("delete");
-        task_delete_el.innerHTML = "Delete";
+        let task_delete_el = createDeleteEl();
 
         task_actions_el.appendChild(task_edit_el);
         task_actions_el.appendChild(task_delete_el);
 
         task_el.appendChild(task_actions_el);
-
 
         list_el.appendChild(task_el);
 
@@ -64,9 +55,58 @@ window.addEventListener('load', () => {
             }
         });
 
-
-        task_delete_el.addEventListener('click', () =>{
+        task_delete_el.addEventListener('click', () => {
             list_el.removeChild(task_el);
         });
     });
 });
+
+
+function createTaskInputElement(task) {
+    let task_input_el = document.createElement('input');
+    task_input_el.classList.add("text");
+    task_input_el.type = "text";
+    task_input_el.value = task;
+    task_input_el.setAttribute("readonly", "readonly");
+
+    return task_input_el;
+}
+
+
+function createTaskEl() {
+    let task_el = document.createElement("div");
+    task_el.classList.add("task");
+
+    return task_el
+}
+
+function createTaskContentEl() {
+    let task_content_el = document.createElement("div");
+    task_content_el.classList.add("content");
+
+    return task_content_el
+}
+
+function createTaskActionEl() {
+    let task_actions_el = document.createElement("div");
+    task_actions_el.classList.add("actions");
+
+    return task_actions_el
+}
+
+function createTaskEditEl() {
+    let task_edit_el = document.createElement("button");
+    task_edit_el.classList.add("edit");
+    task_edit_el.innerHTML = "Edit";
+
+    return task_edit_el
+
+
+    }
+    function createDeleteEl() {
+        let task_delete_el = document.createElement("button");
+        task_delete_el.classList.add("delete");
+        task_delete_el.innerHTML = "Delete";
+
+        return task_delete_el
+}
